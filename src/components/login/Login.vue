e881125e4b3a8d7490f62a5df1b055ff0f05e270
unknown <lewosuoxiu@163.com>
2019-08-06 14:48:38 +0800

    登陆成功是保存返回的token值

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -42,6 +42,9 @@ export default {
           const {data, meta: {msg, status}} = res.data
 
            if (status === 200) {
+            //  登陆成功,保存token值
+            localStorage.setItem('token', data.token)
+            
             //  如果登陆成功：1，跳转home页；2，提示成功
              this.$router.push({name: 'home'}) //编程式导航
              this.$message.success(msg);
