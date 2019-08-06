fada9dc91905922f284d2328348a41b09ce205ab
unknown <lewosuoxiu@163.com>
2019-08-06 19:55:35 +0800

    引入侧边栏导航组件

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -3,24 +3,111 @@
     <el-header class="header">
       <el-row>
         <el-col :span="4">
-          <div class="grid-content bg-purple">
-              <img src="../../assets/pics/cloudMind_pic02.png" width="80%" alt="无法显示图片">
+          <div class="heaeder-pic grid-content bg-purple">
+            <img class src="../../assets/pics/machine.png" width="40%" alt="无法显示图片" />
           </div>
         </el-col>
         <el-col :span="18" class="middle">
-            <h3>药柜管理系统</h3>
-
+          <h3>药柜管理系统</h3>
         </el-col>
         <el-col :span="2">
           <div class="grid-content bg-purple">
-              <a class="loginout" href="#">退出</a>
+            <a class="loginout" href="#">退出</a>
           </div>
         </el-col>
       </el-row>
     </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
-      <el-main class="main">Main</el-main>
+      <!-- 侧边栏 -->
+      <el-aside class="aside" width="200px">
+        <el-menu
+        :unique-opened="true"
+        >
+          <!-- 子菜单 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <!-- 子子菜单 -->
+            <el-menu-item index="1-1">
+              <i class="el-icon-circle-check"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- 子菜单 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <!-- 子子菜单 -->
+            <el-menu-item index="1-1">
+              <i class="el-icon-circle-check"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+              <!-- 子子菜单 -->
+             <el-menu-item index="1-1">
+              <i class="el-icon-circle-check"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- 子菜单 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <!-- 子子菜单 -->
+            <el-menu-item index="1-1">
+              <i class="el-icon-circle-check"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <!-- 子子菜单 -->
+            <el-menu-item index="1-1">
+              <i class="el-icon-circle-check"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <!-- 子子菜单 -->
+            <el-menu-item index="1-1">
+              <i class="el-icon-circle-check"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- 子菜单 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <!-- 子子菜单 -->
+            <el-menu-item index="1-1">
+              <i class="el-icon-circle-check"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- 子菜单 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <!-- 子子菜单 -->
+            <el-menu-item index="1-1">
+              <i class="el-icon-circle-check"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-main class="main">
+        <!-- 主展示区域 -->
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -40,16 +127,23 @@ export default {};
   background-color: #d3dcd6;
   /* height: 1000%; */
 }
+.heaeder-pic {
+  /* margin: 0 auto;
+   */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 .main {
   background-color: #e9eef3;
   height: 100%;
 }
 .middle {
-    text-align: center;
+  text-align: center;
 }
 .loginout {
-    line-height: 60px;
-    text-decoration: none;
-    color: black;
+  line-height: 60px;
+  text-decoration: none;
+  color: black;
 }
 </style>
